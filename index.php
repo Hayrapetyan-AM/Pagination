047c9a37af916549e747a13e5f1003193bd969e0
Hayrapetyan-AM <hayrapetyan2203@gmail.com>
2021-12-25 16:54:18 +0400

    Module is complete
    Pagination Module is complete

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 	<title>modules</title>
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
 </head>
-<body class="bg-dark text-success" style="font-size: 30px;">
+<body class="bg-dark text-white">
 
 <?php
 		  
@@ -15,7 +15,7 @@
 		require_once __DIR__.'\dbConn.php';
 		require_once __DIR__.'\paginateClass.php';
 
-		$test = new Pagination($db, 1);
+		$test = new Pagination($db, 2);
 		$table = "tasks";
 		$test->Paginate($table);
 		
